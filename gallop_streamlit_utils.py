f3bcc596076782d0a6fd82c4a8958ba9a09bd507
Mark Spillman <mspillman@users.noreply.github.com>
2021-05-28 15:20:54 +0100

    Update gallop_streamlit_utils.py

--- gallop_streamlit_utils.py
+++ gallop_streamlit_utils.py
@@ -823,7 +823,7 @@ def show_structure(result, Structure, all_settings, hide_H=True, interval=30):
                                 'stick':{"radius":0.25}}})
         view.addUnitCell()
         view.zoomTo()
-        view.render()
+        #view.render()
         t = view.js()
         return t.startjs + "\n" + t.endjs
     else:
@@ -838,14 +838,12 @@ def show_structure(result, Structure, all_settings, hide_H=True, interval=30):
                         'stick':{"radius":0.25}})
         view.addUnitCell()
         view.zoomTo()
-        view.render()
+        #view.render()
         t = view.js()
 
         html = t.startjs + "\n" + t.endjs
 
         # Now save full cell animation
-        print(len(cifs))
-        print(cifs)
         view = py3Dmol.view()
         view.addModelsAsFrames("\n".join(cifs), 'cif',
                         {"doAssembly" : True,
