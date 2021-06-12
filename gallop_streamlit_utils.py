e5432379cc085ab0053738bbe303fc905ae85e84
Mark Spillman <mspillman@users.noreply.github.com>
2021-06-12 17:53:06 +0100

    Update gallop_streamlit_utils.py

--- gallop_streamlit_utils.py
+++ gallop_streamlit_utils.py
@@ -461,7 +461,8 @@ def sidebar():
         print(filedir)
         # Windows
         if os.name == "nt":
-            lines = ["taskkill /IM \"streamlit.exe\" /F\n",
+            lines = ["taskkill /IM \"streamlit.exe\" /F /T\n",
+                    "taskkill /IM \"Python.exe\" /F\n",
                     "streamlit run " + os.path.join(filedir,
                                                     "gallop_streamlit.py")]
             with open(os.path.join(filedir,"reset.bat"), "w") as reset_script:
