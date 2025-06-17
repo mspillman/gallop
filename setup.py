6d3f5f601d445ae18deebdb327f6bb0753d7be6c
Mark Spillman <mspillman@users.noreply.github.com>
2025-06-17 14:34:19 +0100

    Attempt migration to uv

--- setup.py
+++ setup.py
@@ -14,17 +14,4 @@ setup(
                             'gallop/atomic_scattering_params.json']},
     include_package_data=True,
     scripts=['gallop_streamlit.py', 'gallop.bat'],
-    install_requires=['pymatgen==2023.5.10',
-                        'numpy==1.24.3',
-                        'torch==2.0.0',
-                        'scipy==1.11.2',
-                        'matplotlib',
-                        'torch-optimizer==0.3.0',
-                        'streamlit==1.11.1',
-                        'pandas==1.5.3',
-                        'altair==4',
-                        'py3Dmol==2.0.1.post1',
-                        'mpld3==0.5.9',
-                        ],
-
 )
